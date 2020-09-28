bdc6876ad6d191fb7cddb03b796c881288f42a58
dopusteam <o.safonov@innopolis.ru>
2020-09-28 18:32:33 +0300

    initial commit

---
+++ Dopusteam.Education.AspNetCore.Filters/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Dopusteam.Education.AspNetCore.Filters.Infrastructure.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dopusteam.Education.AspNetCore.Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        [EarlyReturnResourceFilter]
+        public IActionResult Index()
+        {
+            return this.Content("Этого никто не увидит");
+        }
+    }
+}
---
+++ Dopusteam.Education.AspNetCore.Filters/Infrastructure/Filters/EarlyReturnResourceFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Dopusteam.Education.AspNetCore.Filters.Infrastructure.Filters
+{
+    public class EarlyReturnResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.Result = new ContentResult
+            {
+                Content = "Результат из фильтра"
+            };
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+        }
+    }
+
+}
